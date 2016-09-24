2bc5a17aadcaf16976cecf340da21720191747dd
Johannes Bertens <johannes.bertens@live.nl>
2016-09-24 15:52:57 +0200

    Added usages

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ if len(sys.argv) < 2:
 # We are going to store the code in a dict, using the filename as index
 code = {}
 
-# We are going to store teh dependencies in a list, in a dict, using the filename as index
+# We are going to store the dependencies and usages in lists, in dictionaries, using the filename as index
 dependencies = {}
+usages = {}
 
 # The names to look for (filename lowercase minus .ado) are stored in a list
 magic_words = []
@@ -43,20 +44,31 @@ for ado_file in code.keys():
             try:
                 ado_word_s = ado_word.decode(encoding='UTF-8')
                 if ado_word_s.lower() in magic_words:
-                    #print("Found a word: " + ado_word_s + " in file " + ado_file)
+                    #print("Found word: " + ado_word_s + " in file " + ado_file)
+
+                    # Save dependencies
                     current_dependencies = []
                     if ado_file in dependencies.keys():
                         current_dependencies = dependencies[ado_file]
 
-                    if ado_word_s.lower() not in current_dependencies:
-                        current_dependencies.append(ado_word_s.lower())
+                    if ado_word_s not in current_dependencies:
+                        current_dependencies.append(ado_word_s)
                         dependencies[ado_file] = current_dependencies
+
+                    # Save usages
+                    current_usages = []
+                    if ado_word_s in usages.keys():
+                        current_usages = usages[ado_word_s]
+                    
+                    if ado_file not in current_usages:
+                        current_usages.append(ado_file)
+                        usages[ado_word_s] = current_usages
             except:
                 # Always due to some silly accent chars in the source file
                 #print("Found a crazy char: " + str(ado_word) + " in file " + ado_file)
                 pass
 
-# Print the dependencies and save to file
+# Print the dependencies and usages + save them to file 'output.txt'
 with open('output.txt', 'w') as file_out:
     for ado_file in dependencies.keys():
         print("Dependencies for " + ado_file + ":")
@@ -64,3 +76,10 @@ with open('output.txt', 'w') as file_out:
         for dep_word in dependencies[ado_file]:
             print("\t" + str(dep_word))
             file_out.write("\t" + str(dep_word)+"\n")
+
+    for ado_word in usages.keys():
+        print("Usages of " + ado_word + ":")
+        file_out.write("Usages of " + ado_word + ":" + "\n")
+        for used_file in usages[ado_word]:
+            print("\t" + str(used_file))
+            file_out.write("\t" + str(used_file)+"\n")
